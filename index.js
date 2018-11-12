6d54979aca445cd861cea1ac2d496bea07effa32
Cory Kim <github@cory.kim>
2018-11-12 11:23:45 -0600

    refactor(oauth): Use the new library function for oauth2 proxy request

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const q = require('daskeyboard-applet');
 const logger = q.logger;
 
-const request = require('request-promise');
-
 const queryUrlBase = 'https://www.googleapis.com/gmail/v1/users/me/messages?q=';
-const proxyUrl = "http://localhost:8094/thirdparty/proxy/oauth2/proxy";
 
 class GmailAlerts extends q.DesktopApp {
   constructor() {
@@ -20,22 +17,17 @@ class GmailAlerts extends q.DesktopApp {
       throw new Error("No apiKey available.");
     }
 
-    const query = `from:${monitors.join(" OR ")}+is:unread`;
+    const query = `from:${monitors.join("+OR+")}+is:unread`;
     logger.info("Query: " + query);
 
-    const proxyRequest = {
+    const proxyRequest = new q.Oauth2ProxyRequest({
       apiKey: apiKey,
-      url: queryUrlBase + query
-    }
+      uri: queryUrlBase + query
+    });
 
     logger.info("Proxy request: " + JSON.stringify(proxyRequest));
 
-    return request({
-      method: 'POST',
-      uri: proxyUrl,
-      body: proxyRequest,
-      json: true
-    }).then((json) => {
+    return this.oauth2ProxyRequest(proxyRequest).then((json) => {
       logger.info("Got body: " + JSON.stringify(json));
       if (json.messages && json.messages.length > 0) {
         logger.info("Got " + json.messages.length + " messages.");
@@ -46,7 +38,7 @@ class GmailAlerts extends q.DesktopApp {
         return null;
       }
     }).catch((error) => {
-      logger.error("Error while getting mail", error);
+      logger.error("Error while getting mail: " + error);
       throw new Error("Error retrieving email.");
     });
   }
--- test/test.js
+++ test/test.js
@@ -1,21 +1,46 @@
 const assert = require('assert');
 const t = require('../index.js');
+const apiKey = require('./auth.json').apiKey;
 
-describe('GmailAlerts', function() {
-  describe('#constructor()', function() {
-    it('should return a valid instance', function() {
+describe('GmailAlerts', function () {
+  describe('#constructor()', function () {
+    it('should return a valid instance', function () {
       let test = new t.GmailAlerts();
       assert.ok(test);
     })
   });
-  
-  describe('#run()', function() {
-    it('should fail without auth tokens', function() {
+
+  describe('#run()', function () {
+    it('should fail without an API key', function () {
       let test = new t.GmailAlerts();
-      test.run().then((result) => {        
+      test.run().then((result) => {
+        return assert.fail("Should have failed.");
       }).catch((error) => {
-        assert.ok(error);
-      })      
-    })
+        return assert.ok(error);
+      })
+    });
+
+    it('should get a message if it has an API key', function () {
+      console.log("My API key is: " + apiKey);
+      let test = new t.GmailAlerts();
+      test.processConfig({
+        authorization: {
+          apiKey: apiKey
+        },
+        applet: {
+          user: {
+            monitors: ['guillaume@metadot.com', 'daniel@metadot.com']
+          }
+        }
+      }).then(() => {
+        test.run().then((signal) => {
+          console.log("Got signal: " + signal);
+          return assert(signal === null || signal);
+        }).catch(error => {
+          return assert.fail("Got error: " + error);
+        });
+      })
+    });
+
   })
-})
+});
