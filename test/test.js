bfa566c713b1e781fdac98ca4c5299d033a26c72
Cory Kim <github@cory.kim>
2018-12-05 11:57:09 -0600

    change(signal): Only send signals when new messages are received

--- test/test.js
+++ test/test.js
@@ -10,8 +10,33 @@ describe('GmailAlerts', function () {
     })
   });
 
+  describe('#checkMessages()', function () {
+    it('should get messages since an old timestamp', async function () {
+      return buildApp().then(async (app) => {
+        app.timestamp = getOldTimestamp();
+
+        return app.checkMessages().then(json => {
+          assert.ok(json.resultSizeEstimate);
+        });
+      })
+    });
+
+    it('should ignore older messages', async function () {
+      return buildApp().then(async (app) => {
+        app.timestamp = getOldTimestamp();
+
+        return app.checkMessages().then(async (json) => {
+          assert.ok(json.resultSizeEstimate);
+          return app.checkMessages().then(json => {
+            assert.equal(0, json.resultSizeEstimate);
+          });
+        });
+      })
+    });
+  });
+
   describe('#run()', function () {
-    it('should fail without an API key', function () {
+    it('should fail without an API key', async function () {
       let test = new t.GmailAlerts();
       return test.run().then((result) => {
         return assert.fail("Should have failed.");
@@ -20,28 +45,67 @@ describe('GmailAlerts', function () {
       })
     });
 
-    it('should get a message if it has an API key', function () {
-      console.log("My API key is: " + apiKey);
-      let test = new t.GmailAlerts();
-      return test.processConfig({
-        authorization: {
-          apiKey: apiKey
-        },
-        applet: {
-          user: {
-            monitors: ['guillaume@metadot.com', 'daniel@metadot.com', 
-            'cory@metadot.com']
-          }
-        }
-      }).then(() => {
-        return test.run().then((signal) => {
+    it('should get a signal', async function () {
+      return buildApp().then(async (app) => {
+        app.timestamp = getOldTimestamp();
+        return app.run().then((signal) => {
           console.log("Got signal: " + JSON.stringify(signal));
-          return assert(signal === null || signal);
+          return assert.ok(signal);
         }).catch(error => {
           return assert.fail("Got error: " + error);
         });
       })
     });
 
+    it('should not get signal on first run', async function () {
+      return buildApp().then(async (app) => {
+        return app.run().then((signal) => {
+          console.log("Got signal: " + JSON.stringify(signal));
+          return assert.equal(null, signal);
+        }).catch(error => {
+          return assert.fail("Got error: " + error);
+        });
+      })
+    });
+
+    it('should ignore old emails', async function () {
+      return buildApp().then(async (app) => {
+        app.timestamp = getOldTimestamp();
+        return app.run().then((signal) => {
+          console.log("Got signal: " + JSON.stringify(signal));
+          assert.ok(signal);
+        }).then(async () => {
+          return app.run().then((signal) => {
+            console.log("Got signal: " + JSON.stringify(signal));
+            return assert.equal(null, signal);
+          });
+        });
+      }).catch(error => {
+        return assert.fail("Got error: " + error);
+      });
+    })
   })
 });
+
+// returns a 1-year-old timestamp
+function getOldTimestamp() {
+  return t.getTimestamp() - 28800 * 365
+}
+
+async function buildApp() {
+  let test = new t.GmailAlerts();
+  return test.processConfig({
+    authorization: {
+      apiKey: apiKey
+    },
+    applet: {
+      user: {
+        monitors: ['guillaume@metadot.com', 'daniel@metadot.com',
+          'cory@metadot.com', 'gmail@cory.kim'
+        ]
+      }
+    }
+  }).then(() => {
+    return test
+  });
+}
