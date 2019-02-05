067082783f75a223fd03c89517b1af821fefaf14
AhmedLoud <ahmed.loudghiri@gmail.com>
2019-02-05 10:57:45 -0600

    fix(Request): Requests will use daskeyboard proxy only to ask for new tokens

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const q = require('daskeyboard-applet');
+const request = require('request-promise');
 const logger = q.logger;
 
 const queryUrlBase = 'https://www.googleapis.com/gmail/v1/users/me/messages';
@@ -27,37 +28,101 @@ class GmailAlerts extends q.DesktopApp {
   }
 
   async checkMessages() {
+    return this.getGmailAccessToken().then(accessToken => {
+      // save token
+      this.gmailAccessToken = accessToken;
+      return this.getMessagesWithToken(accessToken);
+    }).catch(err => {
+      logger.info(`Gmail: error when checking messages. Will try to refresh token ${err}`);
+
+      return this.refreshGmailAccessToken().then(accessToken => {
+        this.gmailAccessToken = accessToken;
+        return this.getMessagesWithToken(accessToken);
+      });
+    });
+  }
+
+  async getMessagesWithToken(token) {
+    logger.info(`getMessagesWithToken`);
+    if (!this.config.monitors) {
+      return [];
+    }
+    const monitors = this.config.monitors;
     const timestamp = this.timestamp;
     this.timestamp = getTimestamp();
     logger.info(`Checking for monitored messages since ${timestamp}`);
+    let options = {
+      uri: queryUrlBase,
+      qs: {
+        q: `from:${monitors.join(" OR ")} is:unread after: ${timestamp}`
+      },
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      json: true
+    }
+    return request(options);
+  }
 
-    const monitors = this.config.monitors;
+  /**
+   * Use daskeyboard Oauth Proxy to make the request
+   */
+  async getGmailAccessToken() {
+    if (!this.authorization.apiKey) {
+      throw new Error("No apiKey available.");
+    }
 
+    if (this.gmailAccessToken) {
+      return this.gmailAccessToken;
+    }
+
+    const proxyRequest = new q.Oauth2ProxyRequest({
+      apiKey: this.authorization.apiKey
+    });
+
+
+    return proxyRequest.getOauth2ProxyToken().then(proxyResponse => {
+      return proxyResponse.access_token;
+    });
+  }
+
+  /**
+   * Uses daskeyboard Oauth proxy to refresh access token
+   */
+  async refreshGmailAccessToken() {
     if (!this.authorization.apiKey) {
       throw new Error("No apiKey available.");
     }
 
     const proxyRequest = new q.Oauth2ProxyRequest({
-      apiKey: this.authorization.apiKey,
-      uri: queryUrlBase,
-      qs: {
-        q: `from:${monitors.join(" OR ")} is:unread after: ${timestamp}`,
-      }
+      apiKey: this.authorization.apiKey
     });
 
-    return this.oauth2ProxyRequest(proxyRequest);
+    return proxyRequest.refreshOauth2AccessToken().then(proxyResponse => {
+      return proxyResponse.access_token;
+    });
   }
 
   async retrieveMessage(id) {
-    const proxyRequest = new q.Oauth2ProxyRequest({
-      apiKey: this.authorization.apiKey,
+    let options = {
       uri: `${queryUrlBase}/${id}`,
       qs: {
-        fields: `payload/headers`,
-      }
-    });
-
-    return this.oauth2ProxyRequest(proxyRequest);
+        fields: `payload/headers`
+      },
+      json: true
+    }
+    return this.getGmailAccessToken().then(accessToken => {
+      this.gmailAccessToken = accessToken;
+      options = { ...options, headers: { 'Authorization': `Bearer ${accessToken}` } }
+      return request(options);
+    }).catch(err => {
+      logger.info(`Gmail: error when checking messages.Will try to refresh token ${err} `);
+      return this.refreshGmailAccessToken().then(accessToken => {
+        this.gmailAccessToken = accessToken;
+        options = { ...options, headers: { 'Authorization': `Bearer ${accessToken}` } }
+        return request(options);
+      });
+    });;
   }
 
   async generateSignalMessage(json) {
@@ -72,7 +137,7 @@ class GmailAlerts extends q.DesktopApp {
 
     const lines = [];
     for (let sender of senders.sort()) {
-      lines.push(`<div>New message from ${sender}</div>`);
+      lines.push(`<div> New message from ${sender}</div > `);
     }
     return lines.join('\n');
   }
@@ -92,7 +157,7 @@ class GmailAlerts extends q.DesktopApp {
             points: [
               [new q.Point("#00BFFF", q.Effects.BLINK)]
             ],
-            name: `${account}`,
+            name: `${account} `,
             message: message,
             link: {
               url: 'https://mail.google.com',
@@ -104,7 +169,7 @@ class GmailAlerts extends q.DesktopApp {
         return null;
       }
     }).catch((error) => {
-      logger.error("Error while getting mail: " + error);
+      logger.error(`Error while getting mail: ${error} `);
       throw new Error("Error retrieving email.");
     });
   }
